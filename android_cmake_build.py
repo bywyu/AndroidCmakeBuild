e66b6f8b3cac2d7477a48d67dd74d0cdd1c76198
fpgeek <goodoi09@gmail.com>
2012-08-29 17:02:12 +0900

    standalone build success

--- android_cmake_build.py
+++ android_cmake_build.py
@@ -2,37 +2,27 @@
 import ConfigParser, os, shlex, subprocess, sys
     
 def setAndroidPathEnv(configObj):
-    sdkPath, ndkPath = configObj.get('ANDROID', 'SDK_PATH'), configObj.get('ANDROID', 'NDK_PATH')
-    os.putenv('ANDROID_NDK', ndkPath)
-    #os.putenv('ANDROID_SDK', sdkPath)
+    ndkPath = configObj.get('ANDROID', 'NDK_PATH')
+    os.putenv('NDK', ndkPath)
     
     level, arch, toolchainInstallDir = configObj.get('NDK', 'LEVEL'), configObj.get('NDK', 'ARCH'), configObj.get('NDK', 'TOOLCHAIN_INSTALL_DIR')
-    toolchainCommand = '$ANDROID_NDK/build/tools/make-standalone-toolchain.sh --platform=android-%(level)s --install-dir=%(install-dir)s --arch=%(arch)s' % {'level':level, 'arch':arch, 'install-dir':toolchainInstallDir}
-    #subprocess.call(toolchainCommand, shell=True)
+    toolchainCommand = '$NDK/build/tools/make-standalone-toolchain.sh --platform=android-%(level)s --install-dir=%(install-dir)s --arch=%(arch)s' % {'level':level, 'arch':arch, 'install-dir':toolchainInstallDir}
+    subprocess.call(toolchainCommand, shell=True)
     
-    #prePath = os.getenv('PATH')
-    #os.putenv('PATH', '%s/bin:%s' % (toolchainInstallDir, prePath))
-    #os.putenv('CC', getCCName(configObj, arch))
-    
-    #os.putenv('ANDROID_NDK_TOOLCHAIN_ROOT', toolchainInstallDir)
-    #androidCmakePath = configObj.get('ANDROID', 'CMAKE_PATH')
-    #os.putenv('ANDTOOLCHAIN', '%s/toolchain/android.toolchain.cmake' % androidCmakePath)
-    
-    
-    
-def getCCName(configObj, arch):
-    ccNameMap = {'linux':'%s-linux-androideabi-g++' % arch}
-    return ccNameMap.get( configObj.get('NDK', 'HOST_SYSTEM') )
+    os.putenv('ANDROID_STANDALONE_TOOLCHAIN', toolchainInstallDir)    
+
+def getCmakeToolchainFileName(configObj):
+    androidCmakePath = configObj.get('ANDROID', 'CMAKE_PATH')
+    return '%s/toolchain/android.toolchain.cmake' % androidCmakePath
 
 def executeCmake(configObj, args):
-    androidCmakePath = configObj.get('ANDROID', 'CMAKE_PATH')
-    command = 'cmake -DCMAKE_TOOLCHAIN_FILE=%s %s' % ('%s/toolchain/android.toolchain.cmake' % androidCmakePath,  ' '.join(args))
+    command = 'cmake -DCMAKE_TOOLCHAIN_FILE=%s %s' % (getCmakeToolchainFileName(configObj),  ' '.join(args))
     print command
     subprocess.call(command, shell=True)
 
 if __name__ == '__main__':
     if  len(sys.argv) < 2:
-        print 'please input args...'
+        subprocess.call("python android_cmake_build.py [cmake args]", shell=True)
     else:
         configObj = ConfigParser.RawConfigParser()
         configObj.read("config.ini") 
